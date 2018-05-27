2596946ca1c7bce551fd93e024bccbe0fea45d04
Doyouknowkimchi <21700268@handong.edu>
2018-05-27 16:00:39 +0900

    second

--- src/main/java/edu/handong/csee/java/HW3/FileLoader.java
+++ src/main/java/edu/handong/csee/java/HW3/FileLoader.java
@@ -6,30 +6,26 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FileLoader{
 	// 디렉토리  path를 찾아서 file로 만들어서 list file을 통해 파일 쭉 돌릴 수 있다.
+
+	//File.listFiles() 를 이용하면 해당 디렉토리에 있는 모든 파일 정보를 받아 올 수 있습니다.
+
 	
-	void pulldata(String path)
-	{
-		BufferedReader br = null; 
-		PrintWriter pw = null;
-		try{        
-			br = new BufferedReader(new FileReader("경로,파일")); 
-			pw = new PrintWriter(new FileWriter(""));
-			String line = null;
-			while((line = br.readLine())!= null){
-				pw.println(line);
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}finally {
-			pw.close();
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+	ArrayList<String> makefilelist(String path){
+		ArrayList<String> results = new ArrayList<String>();
+		System.out.println("put directory path");
+		File[] files = new File(path).listFiles();
+
+		for (File file : files) {
+			if (file.isFile()) 
+				results.add(file.getName());
 		}
+		System.out.println(results);
+		return results;
 	}
+
+
 }
